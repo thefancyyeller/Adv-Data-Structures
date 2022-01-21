93b48ab8941d2acfd631af71441325a13b58aa90
thefancyyeller <46755406+thefancyyeller@users.noreply.github.com>
2022-01-20 20:07:25 -0600

    Hello World
    Added the skeleton of our project

---
+++ Lab 2/src/MainProgram.java
@@ -0,0 +1,10 @@
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		//this is our main program. Everyone should make their algorithms as functions in this file
+
+	}
+
+}
